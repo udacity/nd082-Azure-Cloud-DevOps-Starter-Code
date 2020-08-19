13f4edabfe059695d34b0130414cac63ab626c6f
Noah Gift <noah.gift@gmail.com>
2020-08-18 20:08:21 -0400

    deleting changes

--- C2-AgileDevelopmentwithAzure/azure_pipelines_exercise/starter_files/test_hello.py
+++
@@ -1,8 +0,0 @@
-from click.testing import CliRunner
-from hello import hello
-
-def test_hello():
-    runner = CliRunner()
-    result = runner.invoke(hello, ["--name", "Thor",
-        "--color", "blue"])
-    assert "Thor" in result.output
