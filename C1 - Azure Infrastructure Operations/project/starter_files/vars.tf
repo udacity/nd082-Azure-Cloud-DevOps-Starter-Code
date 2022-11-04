8e547ee0fcbe51ad0cd68fb0961af6c0eb9ee4ea
HiepMartin <43259525+hiepdv4@users.noreply.github.com>
2022-11-04 09:51:11 +0700

    Update vars.tf

--- C1 - Azure Infrastructure Operations/project/starter_files/vars.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/vars.tf
@@ -1,35 +1,36 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
-  default     = "udacity"
+  default     = "hiepdang"
 }
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
-  default     = "Southeast Asia"
+  default     = "East US"
 }
 
 variable "tags" {
   description = "A map of the tags to use for the resources that are deployed"
   type        = map(string)
   default = {
-    Name = "udacity-azure-webserver"
+    Name = "hiepdang-webserver"
+    Provider = "azure"
   }
 }
 
 variable "resource_group_name" {
   type        = string
   description = "The name of Azure resource group"
-  default     = "udacity-demo-packer-rg"
+  default     = "hiepdang"
 }
 
 variable "admin_username" {
   description = "The Virtual machine admin username"
-  default     = "adminuser"
+  default     = "hiepudacity"
 }
 
 variable "admin_password" {
   description = "The Virtual admin Machine password"
-  default     = "Admin@123"
+  default     = "Abcd@1234"
 }
 
 variable "vm_count" {
@@ -40,5 +41,5 @@ variable "vm_count" {
 
 variable "imageid" {
   description = "The ID of the Packer Image created"
-  default     = "/subscriptions/0bf66a04-23c0-4307-90cd-dae3b7ec4c35/resourceGroups/udacity-demo-packer-rg/providers/Microsoft.Compute/images/udacity-demo-packer-image"
+  default     = "/subscriptions/4f9f2e79-1bd1-496d-b27a-4d875eff0d35/resourceGroups/hiepdang/providers/Microsoft.Compute/images/LinuxImage"
 }
